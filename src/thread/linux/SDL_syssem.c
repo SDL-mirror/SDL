786dde16bacaef573caae4ff948782cf10e2c5cb
Sam Lantinga <slouken@lokigames.com>
2001-05-10 18:31:21 +0000

    Added pthread.h - necessary on some configurations

--- src/thread/linux/SDL_syssem.c
+++ src/thread/linux/SDL_syssem.c
@@ -54,6 +54,7 @@ static char rcsid =
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>			/* For getpid() */
+#include <pthread.h>
 #include <semaphore.h>
 
 /* Wrapper around POSIX 1003.1b semaphores */
