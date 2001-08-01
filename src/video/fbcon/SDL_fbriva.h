d0bad78060dcb8fa685d495824f5664712df0688
Sam Lantinga <slouken@libsdl.org>
2001-08-01 03:23:40 +0000

    Fix compile riva fb problem on older kernels

--- src/video/fbcon/SDL_fbriva.h
+++ src/video/fbcon/SDL_fbriva.h
@@ -29,5 +29,12 @@ static char rcsid =
 
 #include "SDL_fbvideo.h"
 
+#ifndef FB_ACCEL_NV3
+#define FB_ACCEL_NV3	27
+#endif
+#ifndef FB_ACCEL_NV4
+#define FB_ACCEL_NV4	28
+#endif
+
 /* Set up the driver for Riva acceleration */
 extern void FB_RivaAccel(_THIS, __u32 card);
