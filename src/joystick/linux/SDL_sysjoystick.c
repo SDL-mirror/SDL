0247943d34b125fdc60aa0778b23296ba7c9f180
Sam Lantinga <slouken@libsdl.org>
2002-05-20 17:40:55 +0000

    *** empty log message ***

--- src/joystick/linux/SDL_sysjoystick.c
+++ src/joystick/linux/SDL_sysjoystick.c
@@ -35,7 +35,9 @@ static char rcsid =
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <limits.h>		/* For the definition of PATH_MAX */
-
+#ifdef __arm__
+#include <linux/limits.h> /* Arm cross-compiler needs this */
+#endif
 #include <linux/joystick.h>
 #ifdef USE_INPUT_EVENTS
 #include <linux/input.h>
