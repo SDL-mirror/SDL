606a93ada913810d6f23e00982543e094d6f01c1
Sam Lantinga <slouken@libsdl.org>
2003-07-26 20:02:00 +0000

    You can't create a YUV overlay in OpenGL mode

--- src/video/SDL_yuv.c
+++ src/video/SDL_yuv.c
@@ -32,6 +32,7 @@ static char rcsid =
 
 #include "SDL_getenv.h"
 #include "SDL_video.h"
+#include "SDL_error.h"
 #include "SDL_sysvideo.h"
 #include "SDL_yuvfuncs.h"
 #include "SDL_yuv_sw_c.h"
@@ -45,7 +46,10 @@ SDL_Overlay *SDL_CreateYUVOverlay(int w, int h, Uint32 format,
 	const char *yuv_hwaccel;
 	SDL_Overlay *overlay;
 
-	overlay = NULL;
+	if ( (SDL_VideoSurface->flags & SDL_OPENGL) == SDL_OPENGL ) {
+		SDL_SetError("YUV overlays are not supported in OpenGL mode");
+		return NULL;
+	}
 
 	/* Display directly on video surface, if possible */
 	if ( getenv("SDL_VIDEO_YUV_DIRECT") ) {
@@ -55,6 +59,7 @@ SDL_Overlay *SDL_CreateYUVOverlay(int w, int h, Uint32 format,
 			display = SDL_VideoSurface;
 		}
 	}
+	overlay = NULL;
         yuv_hwaccel = getenv("SDL_VIDEO_YUV_HWACCEL");
 	if ( ((display == SDL_VideoSurface) && video->CreateYUVOverlay) &&
 	     (!yuv_hwaccel || (atoi(yuv_hwaccel) > 0)) ) {
