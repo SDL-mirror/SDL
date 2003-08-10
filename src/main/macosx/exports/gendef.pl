6e83dd82586731ddb02ea039fa2064eb54f0913a
Sam Lantinga <slouken@libsdl.org>
2003-08-10 17:48:56 +0000

    Date: Sun, 10 Aug 2003 13:24:45 -0400 From: Darrell Walisser Subject: Re: Updated projects?
    There was one additional patch I forgot to send. I needed to add a few
    exports to the exports file.

--- src/main/macosx/exports/gendef.pl
+++ src/main/macosx/exports/gendef.pl
@@ -2,6 +2,11 @@
 #
 # Program to take a set of header files and generate DLL export definitions
 
+# Export Objective-C classes for Cocoa integration support
+print "\t.objc_class_name_SDL_QuartzWindow\n";
+print "\t.objc_class_name_SDL_QuartzWindowView\n";
+print "\t.objc_class_name_SDL_QuartzWindowDelegate\n";
+
 while ( ($file = shift(@ARGV)) ) {
 	if ( ! defined(open(FILE, $file)) ) {
 		warn "Couldn't open $file: $!\n";
