6849bb80755a536840ad24dc2de5b0b7f52794da
Sam Lantinga <slouken@libsdl.org>
2003-08-30 06:27:01 +0000

    *** empty log message ***

--- src/video/nanox/SDL_nximage.c
+++ src/video/nanox/SDL_nximage.c
@@ -26,6 +26,7 @@
 */
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "SDL_error.h"
 
--- src/video/nanox/SDL_nxvideo.c
+++ src/video/nanox/SDL_nxvideo.c
@@ -26,6 +26,7 @@
 */
 
 #include <stdlib.h>
+#include <string.h>
 
 #include "SDL_video.h"
 #include "SDL_pixels_c.h"
