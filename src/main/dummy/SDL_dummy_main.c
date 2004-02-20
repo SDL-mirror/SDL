f4baa32414fd7668206135c8b39cc8bfbabb559e
Sam Lantinga <slouken@libsdl.org>
2004-02-20 18:18:20 +0000

    *** empty log message ***

--- src/main/dummy/SDL_dummy_main.c
+++ src/main/dummy/SDL_dummy_main.c
@@ -1,11 +1,13 @@
 
 /* Include the SDL main definition header */
 #include "SDL_main.h"
+
 #ifdef main
 #undef main
-#endif
-
 int main(int argc, char *argv[])
 {
 	return(SDL_main(argc, argv));
 }
+#else
+/* Nothing to do on this platform */;
+#endif
