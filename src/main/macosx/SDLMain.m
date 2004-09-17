a760b49b571081ba804bc980bf04f532c4f5a0aa
Sam Lantinga <slouken@libsdl.org>
2004-09-17 13:47:44 +0000

    *** empty log message ***

--- src/main/macosx/SDLMain.m
+++ src/main/macosx/SDLMain.m
@@ -297,10 +297,7 @@ static void CustomApplicationMain (argc, argv)
 /* Main entry point to executable - should *not* be SDL_main! */
 int main (int argc, char **argv)
 {
-
     /* Copy the arguments into a global variable */
-    int i;
-    
     /* This is passed if we are launched by double-clicking */
     if ( argc >= 2 && strncmp (argv[1], "-psn", 4) == 0 ) {
         gArgc = 1;
