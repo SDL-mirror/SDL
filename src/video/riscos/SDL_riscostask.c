ff6c2a4bfd8fa274a7dcb1aa682aa12dea424d31
Sam Lantinga <slouken@libsdl.org>
2005-05-16 07:43:46 +0000

    Date: Tue, 10 May 2005 22:15:25 +0100 From: Peter Naulls Subject: RISC OS SDL patch
    Some of the RISC OS audio code is no longer used do to the switch to
    /dev/dsp, and can be removed as per the attached diff.

--- src/video/riscos/SDL_riscostask.c
+++ src/video/riscos/SDL_riscostask.c
@@ -52,7 +52,6 @@ static int wimp_version = 0;
 /* RISC OS variables to help compatability with certain programs */
 int riscos_backbuffer = 0; /* Create a back buffer in system memory for full screen mode */
 int riscos_closeaction = 1; /* Close icon action */
-int riscos_audiobuffer = 0; /* Audio buffer size */
 
 static int stored_mode = -1; /* -1 when in desktop, mode number or pointer when full screen */
 
@@ -178,13 +177,6 @@ void RISCOS_ExitTask()
                     0 Don't show close icon
                     1 Show close icon
 
-               SDL$<name>$AudioBuffer - set to number of samples to buffer
-                    in advance. Will default to a minimum of 1024 or twice
-                    amount requested by program whichever is largest.
-                    If not specified default is amount for 10 csecs.
-                    Time that will be pre-buffered can be calculated as
-                    sample to buffer * 1000 / freq milliseconds.
-                    
 ***************************************************************************/
 
 int RISCOS_GetTaskName(char *task_name)
@@ -254,13 +246,6 @@ int RISCOS_GetTaskName(char *task_name)
 
 			   env_val = getenv(env_var);
 			   if (env_val && strcmp(env_val,"0") == 0) riscos_closeaction = 0;
-
-			   strcpy(env_var, "SDL$");
-			   strcat(env_var, p);
-			   strcat(env_var, "$AudioBuffer");
-
-			   env_val = getenv(env_var);
-			   riscos_audiobuffer = atoi(env_val);
 
 			   free(env_var);
 		   }
