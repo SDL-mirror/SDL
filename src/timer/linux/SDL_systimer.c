8ea076d0b92eac23baad6b767f3f3d147f5004ab
Ryan C. Gordon <icculus@icculus.org>
2005-10-11 17:33:03 +0000

    Date: Tue, 11 Oct 2005 16:58:12 +0300 (EEST) From: =?ISO-8859-1?Q?Martin_Storsj=F6?= <martin@martin.st> To: sdl@libsdl.org Subject: [SDL] [PATCH] Use nanosleep on OS X
    Hi,
    
    The current version of SDL_Delay on OS X seems to always sleep at least 10
    msec. OS X has nanosleep(), which performs shorter sleeps well. The
    attached patch makes it use that one instead of select() as currently.
    
    // Martin

--- src/timer/linux/SDL_systimer.c
+++ src/timer/linux/SDL_systimer.c
@@ -55,7 +55,7 @@ static char rcsid =
 /* Linux select() changes its timeout parameter upon return to contain
    the remaining time. Most other unixen leave it unchanged or undefined. */
 #define SELECT_SETS_REMAINING
-#elif defined(__bsdi__) || defined(__FreeBSD__) || defined(__sun)
+#elif defined(__bsdi__) || defined(__FreeBSD__) || defined(__sun) || defined(MACOSX)
 #define USE_NANOSLEEP
 #endif
 
