ed75553184c761fe2dcb294f5cb015c842ffdade
Sam Lantinga <slouken@libsdl.org>
2006-01-31 14:59:43 +0000

    Date: Sat, 14 Jan 2006 15:16:01 -0500 From: Andrew Fuller Subject: [SDL] [PATCH] ML-8866 PS2->USB converter
    This converter seems to go by several names --  Super Dual Box, Dual
    USB Joypad, and who knows what else.  Also branded differently with
    different colour cases, etc.  But it seems to all be the same
    internals.  It is a common converter used for StepMania, with several
    posts Googleable trying to make it work in Linux.  I got mine
    yesterday and wanted to play stepmania, so I went ahead and made a
    crude patch for libsdl to split this baby into two logical joysticks.
    
    A couple notes about the patch:
    
    This patch works well for two dance mats hooked up and playing
    stepmania, however the mapping of the other buttons may be off.  I
    have no joystick which uses all the buttons the converter reports, so
    I have no way of testing them.
    
    The name I used 0925:8866 which is the USB ID, and what SDLjoytest-GL
    reported is the name, even though lsusb shows Wisegroup, Ltd MP-8866
    Dual USB Joypad, and the existing virtual joystick mapping uses the
    Wisegroup... name.  Not sure why the discrepency.
    
    I'm not subscribed to this mailing list, so please CC me on any
    comments to this.
    
    -Andrew

--- src/joystick/linux/SDL_sysjoystick.c
+++ src/joystick/linux/SDL_sysjoystick.c
@@ -88,7 +88,21 @@ static struct joystick_logical_values {
    {0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},{0,9},{0,10},{0,11},
    {1,0},{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7},{1,8},{1,9},{1,10},{1,11},
    {2,0},{2,1},{2,2},{2,3},{2,4},{2,5},{2,6},{2,7},{2,8},{2,9},{2,10},{2,11},
-   {3,0},{3,1},{3,2},{3,3},{3,4},{3,5},{3,6},{3,7},{3,8},{3,9},{3,10},{3,11}
+   {3,0},{3,1},{3,2},{3,3},{3,4},{3,5},{3,6},{3,7},{3,8},{3,9},{3,10},{3,11},
+
+/* +71 */
+   /* MP-8866 axes map - map to {logical joystick #, logical axis#} */
+   {0,0},{0,1},{0,2},{0,3},
+   {1,0},{1,1},{1,2},{1,3},
+
+/* +79 */
+   /* MP-8866 hat map - map to {logical joystick #, logical hat #} */
+   {0,0},{1,0},
+
+/* +81 */
+   /* MP-8866 button map - map to {logical joystick #, logical button #} */
+   {0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},{0,9},{0,10},{0,11},
+   {1,0},{1,1},{1,2},{1,3},{1,4},{1,5},{1,6},{1,7},{1,8},{1,9},{1,10},{1,11}
 };
 
 static struct joystick_logical_layout {
@@ -97,12 +111,18 @@ static struct joystick_logical_layout {
         int nballs;
         int nbuttons;
 } joystick_logical_layout[] = {
+/* +0 */
         /* MP-8800 logical layout */
         {5, 1, 0, 12},
         {5, 1, 0, 12},
         {5, 1, 0, 12},
-        {4, 1, 0, 12}
+        {4, 1, 0, 12},
+/* +4 */
+	/* MP-8866 logical layout */
+	{4, 1, 0, 12},
+	{4, 1, 0, 12}
 };
+
 
 /*
    Some USB HIDs show up as a single joystick even though they actually
@@ -130,7 +150,12 @@ static struct joystick_logicalmap {
 } joystick_logicalmap[] = {
         {"WiseGroup.,Ltd MP-8800 Quad USB Joypad", 4, joystick_logical_layout,
          joystick_logical_values, joystick_logical_values+19, NULL,
-         joystick_logical_values+23}
+         joystick_logical_values+23},
+	/* WiseGroup MP-8866 Dual USB Joypad */
+	{"0925:8866", 2, joystick_logical_layout+4,
+	 joystick_logical_values+71, joystick_logical_values+79, NULL,
+	 joystick_logical_values+81}
+
 };
 
 /* find the head of a linked list, given a point in it
