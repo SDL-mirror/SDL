ef2b1e5cc56b8766cd92223e9720c0196796453d
Sam Lantinga <slouken@libsdl.org>
2006-02-19 18:52:51 +0000

    I don't think we need aclocal anymore (comes from the automake package)

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 #
-echo "Generating build information using aclocal and autoconf"
+echo "Generating build information using autoconf"
 echo "This may take a while ..."
 
 # Regenerate configuration files
-(aclocal && autoconf) || exit $?
-(cd test; aclocal; autoconf)
+cp acinclude.m4 aclocal.m4
+autoconf
+# FIXME
+#(cd test; aclocal; autoconf)
 
 # Run configure for this platform
 echo "Now you are ready to run ./configure"
