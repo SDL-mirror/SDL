8043ef85fd644ecdc1f4d0c41fb83dd0020401e0
Sam Lantinga <slouken@libsdl.org>
2006-03-21 07:02:34 +0000

    Show an error message if autoconf isn't found

--- test/autogen.sh
+++ test/autogen.sh
@@ -2,6 +2,11 @@
 #
 # Regenerate configuration files
 cp acinclude.m4 aclocal.m4
+found=false
 for autoconf in autoconf autoconf259
-do if which $autoconf >/dev/null; then $autoconf; break; fi
+do if which $autoconf >/dev/null 2>&1; then $autoconf && found=true; break; fi
 done
+if test x$found = xfalse; then
+    echo "Couldn't find autoconf, aborting"
+    exit 1
+fi
