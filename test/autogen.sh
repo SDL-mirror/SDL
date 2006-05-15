6ca7e9990c66ec75997ff6cb9da6e25c8aa5ae06
Sam Lantinga <slouken@libsdl.org>
2006-05-15 06:39:10 +0000

    Added yet another variant of autoconf to the list

--- test/autogen.sh
+++ test/autogen.sh
@@ -3,7 +3,7 @@
 # Regenerate configuration files
 cp acinclude.m4 aclocal.m4
 found=false
-for autoconf in autoconf autoconf259
+for autoconf in autoconf autoconf259 autoconf-2.59
 do if which $autoconf >/dev/null 2>&1; then $autoconf && found=true; break; fi
 done
 if test x$found = xfalse; then
