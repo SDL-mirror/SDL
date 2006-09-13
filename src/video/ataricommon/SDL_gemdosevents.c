9102f8da68fb239d92cd6c3338f1da65a4018ef2
Patrice Mandin <patmandin@gmail.com>
2006-09-13 21:18:35 +0000

    Factorize keyboard mapping between drivers
    --HG--
    branch : SDL-1.2

--- src/video/ataricommon/SDL_gemdosevents.c
+++ src/video/ataricommon/SDL_gemdosevents.c
@@ -40,34 +40,16 @@
 #include "SDL_ataridevmouse_c.h"
 
 /* To save state of keyboard */
-#define ATARIBIOS_MAXKEYS 128
 
 static unsigned char gemdos_currentkeyboard[ATARIBIOS_MAXKEYS];
 static unsigned char gemdos_previouskeyboard[ATARIBIOS_MAXKEYS];
-static unsigned char gemdos_currentascii[ATARIBIOS_MAXKEYS];
 static SDL_bool use_dev_mouse = SDL_FALSE;
-
-/* Special keys state */
-enum {
-	K_RSHIFT=0,
-	K_LSHIFT,
-	K_CTRL,
-	K_ALT,
-	K_CAPSLOCK,
-	K_CLRHOME,
-	K_INSERT
-};
 
 enum {
 	DEV_BUSY=0,
 	DEV_READY
 };
 
-/* The translation tables from a console scancode to a SDL keysym */
-static SDLKey keymap[ATARIBIOS_MAXKEYS];
-
-static SDL_keysym *TranslateKey(int scancode, int asciicode, SDL_keysym *keysym,
-	SDL_bool pressed);
 static void UpdateSpecialKeys(int special_keys_state);
 
 void AtariGemdos_InitOSKeymap(_THIS)
@@ -78,45 +60,15 @@ void AtariGemdos_InitOSKeymap(_THIS)
 	SDL_memset(gemdos_currentkeyboard, 0, sizeof(gemdos_currentkeyboard));
 	SDL_memset(gemdos_previouskeyboard, 0, sizeof(gemdos_previouskeyboard));
 
-	/* Initialize keymap */
-	for ( i=0; i<ATARIBIOS_MAXKEYS; i++ )
-		keymap[i] = SDLK_UNKNOWN;
-
-	/* Functions keys */
-	for ( i = 0; i<10; i++ )
-		keymap[SCANCODE_F1 + i] = SDLK_F1+i;
-
-	/* Cursor keypad */
-	keymap[SCANCODE_HELP] = SDLK_HELP;
-	keymap[SCANCODE_UNDO] = SDLK_UNDO;
-	keymap[SCANCODE_INSERT] = SDLK_INSERT;
-	keymap[SCANCODE_CLRHOME] = SDLK_HOME;
-	keymap[SCANCODE_UP] = SDLK_UP;
-	keymap[SCANCODE_DOWN] = SDLK_DOWN;
-	keymap[SCANCODE_RIGHT] = SDLK_RIGHT;
-	keymap[SCANCODE_LEFT] = SDLK_LEFT;
-
-	/* Special keys */
-	keymap[SCANCODE_ESCAPE] = SDLK_ESCAPE;
-	keymap[SCANCODE_BACKSPACE] = SDLK_BACKSPACE;
-	keymap[SCANCODE_TAB] = SDLK_TAB;
-	keymap[SCANCODE_ENTER] = SDLK_RETURN;
-	keymap[SCANCODE_DELETE] = SDLK_DELETE;
-	keymap[SCANCODE_LEFTCONTROL] = SDLK_LCTRL;
-	keymap[SCANCODE_LEFTSHIFT] = SDLK_LSHIFT;
-	keymap[SCANCODE_RIGHTSHIFT] = SDLK_RSHIFT;
-	keymap[SCANCODE_LEFTALT] = SDLK_LALT;
-	keymap[SCANCODE_CAPSLOCK] = SDLK_CAPSLOCK;
-
 	use_dev_mouse = (SDL_AtariDevMouse_Open()!=0) ? SDL_TRUE : SDL_FALSE;
 
 	vectors_mask = ATARI_XBIOS_JOYSTICKEVENTS;	/* XBIOS joystick events */
 	if (!use_dev_mouse) {
 		vectors_mask |= ATARI_XBIOS_MOUSEEVENTS;	/* XBIOS mouse events */
 	}
-	if (Getcookie(C_MiNT, &dummy)==C_FOUND) {
+/*	if (Getcookie(C_MiNT, &dummy)==C_FOUND) {
 		vectors_mask = 0;
-	}
+	}*/
 	SDL_AtariXbios_InstallVectors(vectors_mask);
 }
 
@@ -130,15 +82,8 @@ void AtariGemdos_PumpEvents(_THIS)
 
 	while (Cconis()!=DEV_BUSY) {
 		unsigned long key_pressed;
-		unsigned char scancode, asciicode;
-
 		key_pressed=Cnecin();
-
-		asciicode = key_pressed;
-		scancode = key_pressed >> 16;
-
-		gemdos_currentkeyboard[scancode]=0xFF;
-		gemdos_currentascii[scancode]=asciicode;
+		gemdos_currentkeyboard[(key_pressed>>16)&(ATARIBIOS_MAXKEYS-1)]=0xFF;
 	}
 
 	/* Read special keys */
@@ -149,12 +94,12 @@ void AtariGemdos_PumpEvents(_THIS)
 		/* Key pressed ? */
 		if (gemdos_currentkeyboard[i] && !gemdos_previouskeyboard[i])
 			SDL_PrivateKeyboard(SDL_PRESSED,
-				TranslateKey(i, gemdos_currentascii[i], &keysym, SDL_TRUE));
+				SDL_Atari_TranslateKey(i, &keysym, SDL_TRUE));
 			
 		/* Key unpressed ? */
 		if (gemdos_previouskeyboard[i] && !gemdos_currentkeyboard[i])
 			SDL_PrivateKeyboard(SDL_RELEASED,
-				TranslateKey(i, gemdos_currentascii[i], &keysym, SDL_FALSE));
+				SDL_Atari_TranslateKey(i, &keysym, SDL_FALSE));
 	}
 
 	if (use_dev_mouse) {
@@ -164,7 +109,7 @@ void AtariGemdos_PumpEvents(_THIS)
 	}
 
 	/* Will be previous table */
-	SDL_memcpy(gemdos_previouskeyboard, gemdos_currentkeyboard, ATARIBIOS_MAXKEYS);
+	SDL_memcpy(gemdos_previouskeyboard, gemdos_currentkeyboard, sizeof(gemdos_previouskeyboard));
 }
 
 static void UpdateSpecialKeys(int special_keys_state)
@@ -173,7 +118,6 @@ static void UpdateSpecialKeys(int special_keys_state)
 	{	\
 		if (special_keys_state & (1<<(numbit))) { \
 			gemdos_currentkeyboard[scancode]=0xFF; \
-			gemdos_currentascii[scancode]=0; \
 		}	\
 	}
 
@@ -182,26 +126,6 @@ static void UpdateSpecialKeys(int special_keys_state)
 	UPDATE_SPECIAL_KEYS(K_CTRL, SCANCODE_LEFTCONTROL);
 	UPDATE_SPECIAL_KEYS(K_ALT, SCANCODE_LEFTALT);
 	UPDATE_SPECIAL_KEYS(K_CAPSLOCK, SCANCODE_CAPSLOCK);
-}
-
-static SDL_keysym *TranslateKey(int scancode, int asciicode, SDL_keysym *keysym,
-	SDL_bool pressed)
-{
-	/* Set the keysym information */
-	keysym->scancode = scancode;
-
-	if (asciicode)
-		keysym->sym = asciicode;		
-	else
-		keysym->sym = keymap[scancode];
-
-	keysym->mod = KMOD_NONE;
-	keysym->unicode = 0;
-	if (SDL_TranslateUNICODE && pressed) {
-		keysym->unicode = SDL_AtariToUnicodeTable[asciicode];
-	}
-
-	return(keysym);
 }
 
 void AtariGemdos_ShutdownEvents(void)
