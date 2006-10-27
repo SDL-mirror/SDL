106664a5ec731acc4b73a8e106d771a6453a12a9
Ryan C. Gordon <icculus@icculus.org>
2006-10-27 20:18:24 +0000

    Fix for buffer overflow in audio code, when feeding a device that needs  resampling.
    Fixes Bugzilla #298.
    
    --HG--
    branch : SDL-1.2

--- src/audio/SDL_audio.c
+++ src/audio/SDL_audio.c
@@ -590,7 +590,8 @@ int SDL_OpenAudio(SDL_AudioSpec *desired, SDL_AudioSpec *obtained)
 			return(-1);
 		}
 		if ( audio->convert.needed ) {
-			audio->convert.len = desired->size;
+			audio->convert.len = (int) ( ((double) desired->size) /
+                                          audio->convert.len_ratio );
 			audio->convert.buf =(Uint8 *)SDL_AllocAudioMem(
 			   audio->convert.len*audio->convert.len_mult);
 			if ( audio->convert.buf == NULL ) {
