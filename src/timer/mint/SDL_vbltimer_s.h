982014481eae54d7898de34206aee01f482b2d26
Patrice Mandin <patmandin@gmail.com>
2007-06-09 19:58:41 +0000

    Avoid switch to supervisor mode in SDL_GetTicks, by updating system counter from vbl interrupt

--- src/timer/mint/SDL_vbltimer_s.h
+++ src/timer/mint/SDL_vbltimer_s.h
@@ -28,6 +28,8 @@
  *	Patrice Mandin
  */
 
+extern volatile long SDL_Atari_hz200;
+
 /* Functions prototypes */
 extern int SDL_AtariVblInstall(void *newvector);
 extern void SDL_AtariVblUninstall(void *newvector);
