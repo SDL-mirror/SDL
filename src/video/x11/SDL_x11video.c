9032aaaf8c5827663ce8c84405f38c5d5b516290
Ryan C. Gordon <icculus@icculus.org>
2007-07-08 01:50:26 +0000

    Clear the NOFRAME and RESIZABLE flags in the X11 target before setting their  new values, so multiple calls to SetVideoMode() that toggle these will end  up with the right data.
       Fixes Bugzilla #441.
    
    --HG--
    branch : SDL-1.2

--- src/video/x11/SDL_x11video.c
+++ src/video/x11/SDL_x11video.c
@@ -1203,6 +1203,9 @@ SDL_Surface *X11_SetVideoMode(_THIS, SDL_Surface *current,
 		current->pitch = SDL_CalculatePitch(current);
 		X11_ResizeImage(this, current, flags);
 	}
+
+	/* Clear these flags and set them only if they are in the new set. */
+	current->flags &= ~(SDL_RESIZABLE|SDL_NOFRAME);
 	current->flags |= (flags&(SDL_RESIZABLE|SDL_NOFRAME));
 
   done:
