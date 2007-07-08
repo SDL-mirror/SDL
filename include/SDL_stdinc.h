e9a87fd5e2e8465c78ee327434f5054ed6527efd
Sam Lantinga <slouken@libsdl.org>
2007-07-08 14:24:55 +0000

    Fixed bug #451
    SDL_memset4 copies a 32-bit value, not an 8-bit value.

--- include/SDL_stdinc.h
+++ include/SDL_stdinc.h
@@ -279,10 +279,7 @@ extern DECLSPEC void *SDLCALL SDL_memset(void *dst, int c, size_t len);
 #define SDL_zero(x)	SDL_memset(&(x), 0, sizeof((x)))
 #define SDL_zerop(x)	SDL_memset((x), 0, sizeof(*(x)))
 
-/* We can count on memset existing on Mac OS X and being well-tuned. */
-#if defined(__MACH__) && defined(__APPLE__)
-#define SDL_memset4(dst, val, len) memset(dst, val, (len)*4)
-#elif defined(__GNUC__) && defined(i386)
+#if defined(__GNUC__) && defined(i386)
 #define SDL_memset4(dst, val, len)				\
 do {								\
 	int u0, u1, u2;						\
