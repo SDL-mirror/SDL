fef51d48dcfab65145cbc404dffd3ca494325b9e
Sam Lantinga <slouken@libsdl.org>
2007-12-28 18:16:42 +0000

    Date: Wed, 14 Nov 2007 22:20:27 -0500 From: Calvin Vette Subject: Fix to compile SDL-1.2 SVN on OS X Leopard
    I found I needed to add a conditional check for Leopard to compile
    cleanly on 1.2-SVN (20071114):
    
    #include <AudioUnit/AudioUnit.h>
    #ifdef AVAILABLE_MAC_OS_X_VERSION_10_5_AND_LATER
    #include <AudioUnit/AUNTComponent.h>
    #endif
    
    where there is now just:
    #include <AudioUnit/AudioUnit.h>

--- src/audio/macosx/SDL_coreaudio.c
+++ src/audio/macosx/SDL_coreaudio.c
@@ -23,6 +23,9 @@
 
 #include <CoreAudio/CoreAudio.h>
 #include <AudioUnit/AudioUnit.h>
+#ifdef AVAILABLE_MAC_OS_X_VERSION_10_5_AND_LATER
+#include <AudioUnit/AUNTComponent.h>
+#endif
 
 #include "SDL_audio.h"
 #include "../SDL_audio_c.h"
