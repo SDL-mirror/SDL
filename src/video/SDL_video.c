06bfd7d230b17b0c099d16d740aa30570e1282f8
Sam Lantinga <slouken@libsdl.org>
2009-01-12 08:23:47 +0000

    Re-enable the screensaver at exit, just in case it's needed...

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -2375,6 +2375,7 @@ SDL_VideoQuit(void)
     }
     /* Halt event processing before doing anything else */
     SDL_StopEventLoop();
+    SDL_EnableScreenSaver();
 
     /* Clean up the system video */
     for (i = _this->num_displays; i--;) {
