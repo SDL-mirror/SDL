e5150da328bc1b15e44587037ec44cb7ef6db826
Sam Lantinga <slouken@libsdl.org>
2009-01-19 07:35:52 +0000

    Updated for older versions of subversion

--- build-scripts/showrev.sh
+++ build-scripts/showrev.sh
@@ -6,5 +6,7 @@ srcdir=`dirname $0`/..
 
 if [ -d $srcdir/.svn ]; then
     cd $srcdir
-    svnversion -c | sed -e 's,[0-9]*:\([0-9]*\)[A-Z]*,\1,'
+    (svnversion -c 2>/dev/null || svnversion .) | \
+        sed -e 's,\([0-9]*\)[A-Z]*,\1,' \
+            -e 's,[0-9]*:\([0-9]*\)[A-Z]*,\1,'
 fi
