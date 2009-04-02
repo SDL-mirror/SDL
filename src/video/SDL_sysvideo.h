b5ad42362f9ca7a217f3b807c4d49f55aca0447c
Sam Lantinga <slouken@libsdl.org>
2009-04-02 04:06:55 +0000

    Hello.
    This patch provides basic support for video on the Sony PS3
    Linux framebuffer. Scaling, format-conversion, and drawing is
    done from the SPEs, so there is little performance impact to
    PPE applications. This is by no means production quality code,
    but it is a very good start and a good example of how to use the
    PS3's hardware capabilities to accelerate video playback on
    the box.
    
    The driver has been verified to work with ffplay, mplayer and xine.
    This piece of software has been developed at the IBM R&D Lab
    in Boeblingen, Germany and is now returned to the community.
    
    Enjoy !
    
    Signed-off-by: D.Herrendoerfer < d.herrendoerfer [at] de [dot] ibm [dot] com >
    
    --HG--
    branch : SDL-1.2

--- src/video/SDL_sysvideo.h
+++ src/video/SDL_sysvideo.h
@@ -347,6 +347,9 @@ extern VideoBootStrap DirectFB_bootstrap;
 #if SDL_VIDEO_DRIVER_PS2GS
 extern VideoBootStrap PS2GS_bootstrap;
 #endif
+#if SDL_VIDEO_DRIVER_PS3
+extern VideoBootStrap PS3_bootstrap;
+#endif
 #if SDL_VIDEO_DRIVER_GGI
 extern VideoBootStrap GGI_bootstrap;
 #endif
--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -63,6 +63,9 @@ static VideoBootStrap *bootstrap[] = {
 #if SDL_VIDEO_DRIVER_PS2GS
 	&PS2GS_bootstrap,
 #endif
+#if SDL_VIDEO_DRIVER_PS3
+	&PS3_bootstrap,
+#endif
 #if SDL_VIDEO_DRIVER_GGI
 	&GGI_bootstrap,
 #endif
