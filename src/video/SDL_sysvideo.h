2158b7ad24346c779734c938777d34c7fca30b8f
Sam Lantinga <slouken@libsdl.org>
2009-10-10 08:46:06 +0000

    Debian patch: 020_libcaca_new_api.diff
    --HG--
    branch : SDL-1.2

--- src/video/SDL_sysvideo.h
+++ src/video/SDL_sysvideo.h
@@ -407,6 +407,9 @@ extern VideoBootStrap OS2FSLib_bootstrap;
 #if SDL_VIDEO_DRIVER_AALIB
 extern VideoBootStrap AALIB_bootstrap;
 #endif
+#if SDL_VIDEO_DRIVER_CACA
+extern VideoBootStrap CACA_bootstrap;
+#endif
 #if SDL_VIDEO_DRIVER_DUMMY
 extern VideoBootStrap DUMMY_bootstrap;
 #endif
--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -123,6 +123,9 @@ static VideoBootStrap *bootstrap[] = {
 #if SDL_VIDEO_DRIVER_AALIB
 	&AALIB_bootstrap,
 #endif
+#if SDL_VIDEO_DRIVER_CACA
+	&CACA_bootstrap,
+#endif
 #if SDL_VIDEO_DRIVER_DUMMY
 	&DUMMY_bootstrap,
 #endif
