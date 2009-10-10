2b98843157a5f53854f344a411b7ba3a5bc37c44
Mike Gorchak <lestat@i.com.ua>
2009-10-10 13:21:21 +0000

    Created windows must be destroyed at CommonQuit().

--- test/common.c
+++ test/common.c
@@ -1022,6 +1022,12 @@ CommonQuit(CommonState * state)
         SDL_AudioQuit();
     }
     if (state->windows) {
+        int it;
+
+        for (it=0; it<state->num_windows; it++)
+        {
+           SDL_DestroyWindow(state->windows[it]);
+        }
         SDL_free(state->windows);
     }
     SDL_free(state);
