22cd9e3e14a2a16a5fe1494ae617e20069e354b6
Ryan C. Gordon <icculus@icculus.org>
2010-02-12 16:59:34 +0000

    Fixed channel swizzling for ALSA target with 6-channel output.
      Fixes Bugzilla #942.
    
    --HG--
    branch : SDL-1.2

--- src/audio/alsa/SDL_alsa_audio.c
+++ src/audio/alsa/SDL_alsa_audio.c
@@ -264,9 +264,8 @@ static void ALSA_WaitAudio(_THIS)
  */
 #define SWIZ6(T) \
     T *ptr = (T *) mixbuf; \
-    const Uint32 count = (this->spec.samples / 6); \
     Uint32 i; \
-    for (i = 0; i < count; i++, ptr += 6) { \
+    for (i = 0; i < this->spec.samples; i++, ptr += 6) { \
         T tmp; \
         tmp = ptr[2]; ptr[2] = ptr[4]; ptr[4] = tmp; \
         tmp = ptr[3]; ptr[3] = ptr[5]; ptr[5] = tmp; \
