9a5b4a1a847e88e6610c4a87077321aff3745ce2
Sam Lantinga <slouken@libsdl.org>
2010-05-09 19:55:28 -0700

    Add a default revision string if we can't find hg

--- build-scripts/showrev.sh
+++ build-scripts/showrev.sh
@@ -4,5 +4,4 @@
 
 # FIXME: this prints the tip, which isn't useful if you're on a different
 #  branch, or just not sync'd to the tip.
-hg tip --template 'hg-{rev}:{node|short}'
-
+hg tip --template 'hg-{rev}:{node|short}' || echo "hg-0:baadf00d"
