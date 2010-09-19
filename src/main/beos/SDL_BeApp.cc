27aa45979dbe5e5bf7d91ae5f37e56107136b938
Sam Lantinga <slouken@libsdl.org>
2010-09-18 17:16:36 -0700

    Here's the patch we use when building SDL-1.2.14 and 1.2-hg on Haiku, it only affects Haiku specific files so should be ok to apply in hg.
    -Scott McCreary
    HaikuPorts
    
    --HG--
    branch : SDL-1.2

--- src/main/beos/SDL_BeApp.cc
+++ src/main/beos/SDL_BeApp.cc
@@ -39,13 +39,15 @@ static SDL_Thread *SDL_AppThread = NULL;
 
 static int StartBeApp(void *unused)
 {
+  if(!be_app) {
 	BApplication *App;
 
 	App = new BApplication("application/x-SDL-executable");
 
 	App->Run();
 	delete App;
-	return(0);
+  }
+ return(0);
 }
 
 /* Initialize the Be Application, if it's not already started */
@@ -86,7 +88,7 @@ int SDL_InitBeApp(void)
 	++SDL_BeAppActive;
 
 	/* The app is running, and we're ready to go */
-	return(0);
+ return(0);
 }
 
 /* Quit the Be Application, if there's nothing left to do */
