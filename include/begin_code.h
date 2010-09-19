59a374b27d6dbb6f1fd1680557f7d20fe165380b
Sam Lantinga <slouken@libsdl.org>
2010-09-19 08:56:53 -0700

    This patch fixes a dllexport directive warning on Haiku
    Scott McCreary
    HaikuPorts
    
    --HG--
    branch : SDL-1.2

--- include/begin_code.h
+++ include/begin_code.h
@@ -43,7 +43,7 @@
 #ifndef DECLSPEC
 # if defined(__BEOS__) || defined(__HAIKU__)
 #  if defined(__GNUC__)
-#   define DECLSPEC	__declspec(dllexport)
+#   define DECLSPEC
 #  else
 #   define DECLSPEC	__declspec(export)
 #  endif
