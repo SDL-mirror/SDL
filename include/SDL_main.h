6c961a2344a423d0675a01f58aea7d2c442a5f7d
Sam Lantinga <slouken@libsdl.org>
2011-01-04 11:39:04 -0800

    Android needs the SDL_main() redirect as well

--- include/SDL_main.h
+++ include/SDL_main.h
@@ -33,7 +33,8 @@
 
 #if defined(__WIN32__) || \
     (defined(__MWERKS__) && !defined(__BEOS__)) || \
-    defined(__SYMBIAN32__) || defined(__IPHONEOS__)
+    defined(__SYMBIAN32__) || defined(__IPHONEOS__) || \
+    defined(__ANDROID__)
 
 #ifdef __cplusplus
 #define C_LINKAGE	"C"
