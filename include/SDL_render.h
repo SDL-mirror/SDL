4360f9d15afe56d936f64b7b8ba7a07c79ebebe7
Sam Lantinga <slouken@libsdl.org>
2011-02-03 10:03:55 -0800

    Added an intro to the features and goals of the 2D rendering API.

--- include/SDL_render.h
+++ include/SDL_render.h
@@ -24,13 +24,29 @@
  *  \file SDL_render.h
  *  
  *  Header file for SDL 2D rendering functions.
+ *
+ *  This API supports the following features:
+ *      * single pixel points
+ *      * single pixel lines
+ *      * filled rectangles
+ *      * texture images
+ *
+ *  The primitives may be drawn in opaque, blended, or additive modes.
+ *
+ *  The texture images may be drawn in opaque, blended, or additive modes.
+ *  They can have an additional color tint or alpha modulation applied to
+ *  them, and may also be stretched with linear interpolation.
+ *
+ *  This API is designed to accelerate simple 2D operations. You may
+ *  want more functionality such as rotation and particle effects and
+ *  in that case you should use SDL's OpenGL/Direct3D support or one
+ *  of the many good 3D engines.
  */
 
 #ifndef _SDL_render_h
 #define _SDL_render_h
 
 #include "SDL_stdinc.h"
-//#include "SDL_pixels.h"
 #include "SDL_rect.h"
 #include "SDL_video.h"
 
