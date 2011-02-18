6ec7646870f77ba0b6a426f8d0a4a0d77c4e9f75
Sam Lantinga <slouken@libsdl.org>
2011-02-18 11:19:34 -0800

    Added a better way to include build rules in the Makefile Cleaned up dependencies on generating SDL_revision.h Fixed 'make install' if you are not building from a Mercurial repository

--- build-scripts/updaterev.sh
+++ build-scripts/updaterev.sh
@@ -7,7 +7,7 @@ cd `dirname $0`
 srcdir=..
 header=$outdir/include/SDL_revision.h
 
-rev=`sh showrev.sh`
+rev=`sh showrev.sh 2>/dev/null`
 if [ "$rev" != "" -a "$rev" != "hg-0:baadf00d" ]; then
     echo "#define SDL_REVISION \"$rev\"" >$header.new
     if diff $header $header.new >/dev/null 2>&1; then
