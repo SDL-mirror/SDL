6196aa221ee46b4a25843c383e216410aff7826f
Sam Lantinga <slouken@libsdl.org>
2011-04-08 13:03:26 -0700

    SDL 1.3 is now under the zlib license.

--- test/common.c
+++ test/common.c
@@ -1,3 +1,14 @@
+/*
+  Copyright (C) 1997-2011 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely.
+*/
 
 /* A simple test program framework */
 
