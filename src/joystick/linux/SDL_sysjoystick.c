d4bb6faea5b9f058fdd785e765d8b518704285a1
Ryan C. Gordon <icculus@icculus.org>
2011-07-13 17:36:29 -0700

    Linux: Search a smaller set of potential joystick axes.
    Newer kernels seem to report bogus axes in the higher ranges, for example
     with a standard PlayStation 3 controller plugged in via USB.
    
    --HG--
    branch : SDL-1.2

--- src/joystick/linux/SDL_sysjoystick.c
+++ src/joystick/linux/SDL_sysjoystick.c
@@ -703,7 +703,7 @@ static SDL_bool EV_ConfigJoystick(SDL_Joystick *joystick, int fd)
 				++joystick->nbuttons;
 			}
 		}
-		for ( i=0; i<ABS_MAX; ++i ) {
+		for ( i=0; i<ABS_MISC; ++i ) {
 			/* Skip hats */
 			if ( i == ABS_HAT0X ) {
 				i = ABS_HAT3Y;
