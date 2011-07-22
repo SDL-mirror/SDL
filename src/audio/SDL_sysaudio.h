fa2029820d86d96b5c51c727592b8873fe792f45
Ryan C. Gordon <icculus@icculus.org>
2011-07-22 00:09:58 -0700

    Don't use a bitfield for this.
    It pads out to an int anyhow, but causes code bloat as the compiler tries to
     mask and shift for that specific bit.

--- src/audio/SDL_sysaudio.h
+++ src/audio/SDL_sysaudio.h
@@ -120,7 +120,7 @@ typedef struct AudioBootStrap
     const char *name;
     const char *desc;
     int (*init) (SDL_AudioDriverImpl * impl);
-    int demand_only:1;          /* 1==request explicitly, or it won't be available. */
+    int demand_only;  /* 1==request explicitly, or it won't be available. */
 } AudioBootStrap;
 
 #endif /* _SDL_sysaudio_h */
