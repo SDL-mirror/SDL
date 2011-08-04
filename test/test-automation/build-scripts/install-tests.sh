74d2cb937adf1486b48c429c1372bd29f509b27d
Markus Kauppila <markus.kauppila@gmail.com>
2011-08-04 20:52:39 +0300

    Added empty test suite for render tests.

--- test/test-automation/build-scripts/install-tests.sh
+++ test/test-automation/build-scripts/install-tests.sh
@@ -16,7 +16,7 @@ elif [[ $PLATFORM ==  "Darwin" ]]; then
 fi
 
 # TODO: put the test in an array
-for suite in "testdummy" "testplatform" "testrect" "testaudio" "testsurface" "testrwops"
+for suite in "testdummy" "testplatform" "testrect" "testaudio" "testsurface" "testrwops" "testrender"
 do
 	cp -f "$suite/.libs/lib$suite.$EXT" $DIRECTORY
 done
