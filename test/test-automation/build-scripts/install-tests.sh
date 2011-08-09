830a156c74c13de20f3bfa19217f057a665e3b03
Markus Kauppila <markus.kauppila@gmail.com>
2011-08-09 18:40:14 +0300

    Added test suites testevent and testkeyboard.

--- test/test-automation/build-scripts/install-tests.sh
+++ test/test-automation/build-scripts/install-tests.sh
@@ -15,7 +15,7 @@ elif [[ $PLATFORM ==  "Darwin" ]]; then
 fi
 
 # TODO: put the test in an array
-for suite in "testdummy" "testplatform" "testrect" "testaudio" "testsurface" "testrwops" "testrender" "testvideo" "testsyswm" "testclipboard"
+for suite in "testdummy" "testplatform" "testrect" "testaudio" "testsurface" "testrwops" "testrender" "testvideo" "testsyswm" "testclipboard" "testevents" "testkeyboard"
 do
 	cp -f "$DIRECTORY/$suite/.libs/lib$suite.$EXT" $DIRECTORY
 done
