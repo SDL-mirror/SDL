d58d3cf9d0e4e93818c41be3f1f5e15eefbf2f40
Ryan C. Gordon <icculus@icculus.org>
2011-08-23 16:47:22 -0400

    Patched to compile on Windows.

--- src/video/windows/SDL_windowsopengl.c
+++ src/video/windows/SDL_windowsopengl.c
@@ -466,7 +466,7 @@ WIN_GL_SetupWindow(_THIS, SDL_Window * window)
         *iAttr++ = _this->gl_config.multisamplesamples;
     }
 
-    if ( this->gl_config.accelerated >= 0 ) {
+    if (_this->gl_config.accelerated >= 0) {
         *iAttr++ = WGL_ACCELERATION_ARB;
         *iAttr++ = (_this->gl_config.accelerated ? WGL_FULL_ACCELERATION_ARB :
                                                    WGL_NO_ACCELERATION_ARB);
