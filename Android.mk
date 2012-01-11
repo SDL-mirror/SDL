9dc347480516114a77fafcb104241bd031b802d9
Sam Lantinga <slouken@libsdl.org>
2012-01-10 21:07:09 -0500

    David Carre cpasjuste@gmail.com
    I'm playing with SDL on android, and did notice a problem in latest sources ( branch "default" ) :
    
    SDL/src/render/opengles/SDL_glesfuncs.h:10: error: 'glDrawTexiOES' undeclared (first use in this function)
    SDL/src/render/opengles/SDL_glesfuncs.h:10: error: (Each undeclared identifier is reported only once
    
    If it can help you win some time here is the fix, applied to the "Android.mk" file :
    
    LOCAL_CFLAGS += -D GL_GLEXT_PROTOTYPES

--- Android.mk
+++ Android.mk
@@ -40,6 +40,7 @@ LOCAL_SRC_FILES := \
 	$(wildcard $(LOCAL_PATH)/src/video/*.c) \
 	$(wildcard $(LOCAL_PATH)/src/video/android/*.c))
 
+LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES
 LOCAL_LDLIBS := -ldl -lGLESv1_CM -lGLESv2 -llog
 
 include $(BUILD_SHARED_LIBRARY)
