e2df95c6f2e7d54ca04278c39e1f2855b40326fb
Sam Lantinga <slouken@libsdl.org>
2012-01-14 00:49:25 -0500

    Fixed inline assembly warning for PPC input constraint with a matching output constraint of incompatible type

--- include/SDL_endian.h
+++ include/SDL_endian.h
@@ -91,10 +91,10 @@ SDL_Swap16(Uint16 x)
 static __inline__ Uint16
 SDL_Swap16(Uint16 x)
 {
-    Uint16 result;
+    int result;
 
   __asm__("rlwimi %0,%2,8,16,23": "=&r"(result):"0"(x >> 8), "r"(x));
-    return result;
+    return (Uint16)result;
 }
 #elif defined(__GNUC__) && (defined(__M68000__) || defined(__M68020__)) && !defined(__mcoldfire__)
 static __inline__ Uint16
