03410caaaf5b719aa0907949203555eb05d73aa8
Sam Lantinga <slouken@libsdl.org>
2012-08-12 23:10:16 -0700

    Renamed SDL_GL_CONTEXT_PROFILE_ES2 to SDL_GL_CONTEXT_PROFILE_ES

--- include/SDL_video.h
+++ include/SDL_video.h
@@ -193,8 +193,7 @@ typedef enum
 {
     SDL_GL_CONTEXT_PROFILE_CORE           = 0x0001,
     SDL_GL_CONTEXT_PROFILE_COMPATIBILITY  = 0x0002,
-    SDL_GL_CONTEXT_PROFILE_ES             = 0x0004,
-    SDL_GL_CONTEXT_PROFILE_ES2            = 0x0004
+    SDL_GL_CONTEXT_PROFILE_ES             = 0x0004
 } SDL_GLprofile;
 
 typedef enum
