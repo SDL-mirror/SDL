189c2463e3bae8bde0ebc037f7e3d9dd9ac284f1
Ryan C. Gordon <icculus@icculus.org>
2012-08-15 02:52:42 -0400

    Minor typo fix.
    --HG--
    branch : SDL-1.2

--- src/events/SDL_events.c
+++ src/events/SDL_events.c
@@ -237,7 +237,7 @@ int SDL_StartEventLoop(Uint32 flags)
 	SDL_EventOK = NULL;
 	SDL_memset(SDL_ProcessEvents,SDL_ENABLE,sizeof(SDL_ProcessEvents));
 	SDL_eventstate = ~0;
-	/* It's not save to call SDL_EventState() yet */
+	/* It's not safe to call SDL_EventState() yet */
 	SDL_eventstate &= ~(0x00000001 << SDL_SYSWMEVENT);
 	SDL_ProcessEvents[SDL_SYSWMEVENT] = SDL_IGNORE;
 
