1f746f70019ac5a27648928fcff1ec14141a655d
David Ludwig <dludwig@pobox.com>
2012-11-04 09:02:58 -0500

    WinRT: added key event support for most WinRT/VirtualKey-based keys

--- src/video/windowsrt/SDL_WinRTApp.h
+++ src/video/windowsrt/SDL_WinRTApp.h
@@ -39,6 +39,8 @@ protected:
 	void OnPointerPressed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
 	void OnPointerReleased(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
 	void OnPointerMoved(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
+    void OnKeyDown(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::KeyEventArgs^ args);
+    void OnKeyUp(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::KeyEventArgs^ args);
 
 private:
 	CubeRenderer^ m_renderer;
