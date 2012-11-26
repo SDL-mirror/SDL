bbc13a8ecbaeca4e9436daa3cb4aa7edfe586fbb
David Ludwig <dludwig@pobox.com>
2012-11-25 19:05:56 -0500

    WinRT: video code cleanup: combined files Direct3DBase.* and SDL_winrtrenderer.*

--- src/video/windowsrt/SDL_winrtrenderer.h
+++ src/video/windowsrt/SDL_winrtrenderer.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 
-#include "Direct3DBase.h"
+#include "DirectXHelper.h"
+#include "SDL.h"
 
 struct VertexPositionColor
 {
@@ -8,22 +9,32 @@ struct VertexPositionColor
 	DirectX::XMFLOAT2 tex;
 };
 
-// This class renders a simple spinning cube.
-ref class SDL_winrtrenderer sealed : public Direct3DBase
+// Helper class that initializes DirectX APIs for 3D rendering.
+ref class SDL_winrtrenderer
 {
-public:
+internal:
 	SDL_winrtrenderer();
 
-	// Direct3DBase methods.
-	virtual void CreateDeviceResources() override;
+public:
+	virtual void Initialize(Windows::UI::Core::CoreWindow^ window);
+	virtual void HandleDeviceLost();
+	virtual void CreateDeviceResources();
+	virtual void CreateWindowSizeDependentResources();
+	virtual void UpdateForWindowSizeChange();
+	virtual void Present();
+	virtual float ConvertDipsToPixels(float dips);
 
 internal:
-	virtual void Render(SDL_Surface * surface, SDL_Rect * rects, int numrects) override;
+	virtual void Render(SDL_Surface * surface, SDL_Rect * rects, int numrects);
     void ResizeMainTexture(int w, int h);
 
-private:
-	bool m_loadingComplete;
-
+protected private:
+	// Direct3D Objects.
+	Microsoft::WRL::ComPtr<ID3D11Device1> m_d3dDevice;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext1> m_d3dContext;
+	Microsoft::WRL::ComPtr<IDXGISwapChain1> m_swapChain;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> m_renderTargetView;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> m_depthStencilView;
 	Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
 	Microsoft::WRL::ComPtr<ID3D11Buffer> m_vertexBuffer;
 	Microsoft::WRL::ComPtr<ID3D11VertexShader> m_vertexShader;
@@ -32,5 +43,17 @@ private:
 	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> m_mainTextureResourceView;
 	Microsoft::WRL::ComPtr<ID3D11SamplerState> m_mainSampler;
 
+	// Cached renderer properties.
+	D3D_FEATURE_LEVEL m_featureLevel;
+	Windows::Foundation::Size m_renderTargetSize;
+	Windows::Foundation::Rect m_windowBounds;
+	Platform::Agile<Windows::UI::Core::CoreWindow> m_window;
+	Windows::Graphics::Display::DisplayOrientations m_orientation;
 	uint32 m_vertexCount;
+
+	// Transform used for display orientation.
+	DirectX::XMFLOAT4X4 m_orientationTransform3D;
+
+    // Has the renderer finished loading?
+    bool m_loadingComplete;
 };
