e7e059a046ef608ed89a168a2b9f9116245cc78b
Andreas Schiffler <aschiffler@ferzkopp.net>
2013-03-10 16:28:20 -0700

    Fix bug 1574 - updaterev.sh fails when path contains spaces

--- build-scripts/updaterev.sh
+++ build-scripts/updaterev.sh
@@ -10,11 +10,11 @@ header=$outdir/include/SDL_revision.h
 rev=`sh showrev.sh 2>/dev/null`
 if [ "$rev" != "" -a "$rev" != "hg-0:baadf00d" ]; then
     revnum=`echo $rev | sed 's,hg-\([0-9]*\).*,\1,'`
-    echo "#define SDL_REVISION \"$rev\"" >$header.new
-    echo "#define SDL_REVISION_NUMBER $revnum" >>$header.new
+    echo "#define SDL_REVISION \"$rev\"" >"$header.new"
+    echo "#define SDL_REVISION_NUMBER $revnum" >>"$header.new"
     if diff $header $header.new >/dev/null 2>&1; then
-        rm $header.new
+        rm "$header.new"
     else
-        mv $header.new $header
+        mv "$header.new" "$header"
     fi
 fi
