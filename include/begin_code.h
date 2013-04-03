7635069643672c80e4b4bcca10fb1ac1f318722c
Ryan C. Gordon <icculus@icculus.org>
2013-04-03 11:58:04 -0400

    Fixed compiler warnings in Mac Xcode builds.

--- include/begin_code.h
+++ include/begin_code.h
@@ -128,12 +128,14 @@
 #define __inline__
 #endif
 
+#ifndef SDL_FORCE_INLINE
 #if defined(_MSC_VER)
 #define SDL_FORCE_INLINE __forceinline
 #elif ( (defined(__GNUC__) && (__GNUC__ >= 4)) || defined(__clang__) )
 #define SDL_FORCE_INLINE __attribute__((always_inline)) static inline
 #else
 #define SDL_FORCE_INLINE static __inline__
+#endif
 #endif
 
 /* Apparently this is needed by several Windows compilers */
