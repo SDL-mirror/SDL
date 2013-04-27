b281192487f135c87a900ec1a407e3004d156fea
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2013-04-27 17:17:37 +0200

    Removed not needed import statements from Java file.

--- android-project/src/org/libsdl/app/SDLActivity.java
+++ android-project/src/org/libsdl/app/SDLActivity.java
@@ -3,7 +3,6 @@ package org.libsdl.app;
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.egl.EGLContext;
-import javax.microedition.khronos.opengles.GL10;
 import javax.microedition.khronos.egl.*;
 
 import android.app.*;
@@ -17,13 +16,8 @@ import android.widget.AbsoluteLayout;
 import android.os.*;
 import android.util.Log;
 import android.graphics.*;
-import android.text.method.*;
-import android.text.*;
 import android.media.*;
 import android.hardware.*;
-import android.content.*;
-
-import java.lang.*;
 
 
 /**
