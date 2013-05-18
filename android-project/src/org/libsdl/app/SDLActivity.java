1cdf7f698895da676213eebf6c0c9f92209b0050
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2013-05-18 14:03:45 +0200

    Changed import statements in Java file.

--- android-project/src/org/libsdl/app/SDLActivity.java
+++ android-project/src/org/libsdl/app/SDLActivity.java
@@ -3,7 +3,8 @@ package org.libsdl.app;
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.egl.EGLContext;
-import javax.microedition.khronos.egl.*;
+import javax.microedition.khronos.egl.EGLDisplay;
+import javax.microedition.khronos.egl.EGLSurface;
 
 import android.app.*;
 import android.content.*;
