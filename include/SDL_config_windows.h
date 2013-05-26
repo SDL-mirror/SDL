1e9722a01662ddbb9948e75eaa19c38e459dbfbf
Sam Lantinga <slouken@libsdl.org>
2013-05-26 11:39:19 -0700

    Don't assume the XAudio2 APIs will never be available

--- include/SDL_config_windows.h
+++ include/SDL_config_windows.h
@@ -142,9 +142,7 @@ typedef unsigned int uintptr_t;
 
 /* Enable various audio drivers */
 #define SDL_AUDIO_DRIVER_DSOUND 1
-#ifndef __GNUC__
 #define SDL_AUDIO_DRIVER_XAUDIO2    1
-#endif
 #define SDL_AUDIO_DRIVER_WINMM  1
 #define SDL_AUDIO_DRIVER_DISK   1
 #define SDL_AUDIO_DRIVER_DUMMY  1
