1a101f252b11dd73ffc13f74e5c4b2287b28762a
Philipp Wiesemann <philipp.wiesemann@arcor.de>
2013-07-06 16:05:09 +0200

    Fixed SDL's implementation of isspace() to check form feed and vertical tab.

--- src/stdlib/SDL_stdlib.c
+++ src/stdlib/SDL_stdlib.c
@@ -182,7 +182,7 @@ int SDL_toupper(int x) { return toupper(x); }
 int SDL_tolower(int x) { return tolower(x); }
 #else
 int SDL_isdigit(int x) { return ((x) >= '0') && ((x) <= '9'); }
-int SDL_isspace(int x) { return ((x) == ' ') || ((x) == '\t') || ((x) == '\r') || ((x) == '\n'); }
+int SDL_isspace(int x) { return ((x) == ' ') || ((x) == '\t') || ((x) == '\r') || ((x) == '\n') || ((x) == '\f') || ((x) == '\v'); }
 int SDL_toupper(int x) { return ((x) >= 'a') && ((x) <= 'z') ? ('A'+((x)-'a')) : (x); }
 int SDL_tolower(int x) { return ((x) >= 'A') && ((x) <= 'Z') ? ('a'+((x)-'A')) : (x); }
 #endif
