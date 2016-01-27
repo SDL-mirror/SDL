1aadebfa48e66c8a1c758653618b8e0ab816f9e2
capehill <16106175+capehill@users.noreply.github.com>
2016-01-27 20:16:19 +0000

    Mouse double-click configuration

--- src/video/amigaos4/SDL_os4mouse.c
+++ src/video/amigaos4/SDL_os4mouse.c
@@ -23,17 +23,104 @@
 #if SDL_VIDEO_DRIVER_AMIGAOS4
 
 #include "SDL_os4video.h"
+#include "../../events/SDL_mouse_c.h"
 
+#define DEBUG
+#include "../../main/amigaos4/SDL_os4debug.h"
+
+static Uint32
+OS4_GetDoubleClickTimeInMillis(_THIS)
+{
+	struct Preferences preferences;
+	Uint32 interval;
+
+	IIntuition->GetPrefs(&preferences, sizeof(preferences));
+	
+	interval =  preferences.DoubleClick.Seconds * 1000 +
+				preferences.DoubleClick.Microseconds / 1000;
+
+	dprintf("Doubleclick time %d ms\n", interval);
+
+	return interval;
+}
+
+static SDL_Cursor*
+OS4_CreateDefaultCursor()
+{
+	return NULL;
+}
+
+static SDL_Cursor*
+OS4_CreateCursor(SDL_Surface * surface, int hot_x, int hot_y)
+{
+	return NULL;
+}
+
+static SDL_Cursor*
+OS4_CreateSystemCursor(SDL_SystemCursor id)
+{
+	return NULL;
+}
+
+static int
+OS4_ShowCursor(SDL_Cursor * cursor)
+{
+	return 0;
+}
+
+/*
+static void
+OS4_MoveCursor(SDL_Cursor * cursor)
+{
+	called for mouse motion
+}
+*/
+
+static void
+OS4_FreeCursor(SDL_Cursor * cursor)
+{
+}
+
+static void
+OS4_WarpMouse(SDL_Window * window, int x, int y)
+{
+}
+
+static int
+OS4_SetRelativeMouseMode(SDL_bool enabled)
+{
+	return 0;
+}
 
 void
 OS4_InitMouse(_THIS)
 {
 	//SDL_VideoData *data = (SDL_VideoData *) _this->driverdata;
+
+	SDL_Mouse *mouse = SDL_GetMouse();
+
+	mouse->CreateCursor = OS4_CreateCursor;
+	mouse->CreateSystemCursor = OS4_CreateSystemCursor;
+	mouse->ShowCursor = OS4_ShowCursor;
+	mouse->FreeCursor = OS4_FreeCursor;
+	mouse->WarpMouse = OS4_WarpMouse;
+	mouse->SetRelativeMouseMode = OS4_SetRelativeMouseMode;
+
+	SDL_SetDefaultCursor( OS4_CreateDefaultCursor() );
+
+	SDL_SetDoubleClickTime( OS4_GetDoubleClickTimeInMillis(_this) );
 }
 
 void
 OS4_QuitMouse(_THIS)
 {
+    SDL_Mouse *mouse = SDL_GetMouse();
+
+    if ( mouse->def_cursor ) {
+        SDL_free(mouse->def_cursor);
+        mouse->def_cursor = NULL;
+        mouse->cur_cursor = NULL;
+    }
 }
 
 #endif /* SDL_VIDEO_DRIVER_AMIGAOS4 */
