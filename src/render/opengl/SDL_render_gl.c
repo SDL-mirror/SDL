6a31873e963c446f4f8c09c8d13b10e96a3924e4
capehill <16106175+capehill@users.noreply.github.com>
2016-02-29 20:02:40 +0000

    Alpha blending fix

--- src/render/opengl/SDL_render_gl.c
+++ src/render/opengl/SDL_render_gl.c
@@ -1039,15 +1039,17 @@ GL_SetBlendMode(GL_RenderData * data, int blendMode)
         case SDL_BLENDMODE_BLEND:
             data->glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
             data->glEnable(GL_BLEND);
-            data->glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+            data->glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             break;
         case SDL_BLENDMODE_ADD:
             data->glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-            data->glDisable(GL_BLEND); /* TODO */
+            data->glEnable(GL_BLEND); /* TODO */
+            data->glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             break;
         case SDL_BLENDMODE_MOD:
             data->glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
-            data->glDisable(GL_BLEND); /* TODO */
+            data->glEnable(GL_BLEND); /* TODO */
+            data->glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             break;
         }
         data->current.blendMode = blendMode;
