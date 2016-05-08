47352883af875b9149b6c955cd16a815c1602645
capehill <16106175+capehill@users.noreply.github.com>
2016-05-08 16:14:13 +0000

    Support for cacheline size + RAM amount queries

--- src/cpuinfo/SDL_cpuinfo.c
+++ src/cpuinfo/SDL_cpuinfo.c
@@ -253,12 +253,12 @@ CPU_haveAltiVec(void)
     if (0 == error)
         altivec = (hasVectorUnit != 0);
 #elif defined __amigaos4__
-	{
-		uint32 vec_unit;
+    {
+        uint32 vec_unit;
 
-		IExec->GetCPUInfoTags(GCIT_VectorUnit, &vec_unit, TAG_DONE);
-		altivec = (vec_unit == VECTORTYPE_ALTIVEC);
-	}
+        IExec->GetCPUInfoTags(GCIT_VectorUnit, &vec_unit, TAG_DONE);
+        altivec = (vec_unit == VECTORTYPE_ALTIVEC);
+    }
 #elif SDL_ALTIVEC_BLITTERS && HAVE_SETJMP
     void (*handler) (int sig);
     handler = signal(SIGILL, illegal_instruction);
@@ -528,8 +528,15 @@ SDL_GetCPUCacheLineSize(void)
         cpuid(0x80000005, a, b, c, d);
         return (c & 0xff);
     } else {
+#ifdef __AMIGAOS4__
+        uint32 size;
+
+        IExec->GetCPUInfoTags(GCIT_CacheLineSize, &size, TAG_DONE);
+        return size;
+#else
         /* Just make a guess here... */
         return SDL_CACHELINE_SIZE;
+#endif
     }
 }
 
@@ -702,6 +709,11 @@ SDL_GetSystemRAM(void)
             if (GlobalMemoryStatusEx(&stat)) {
                 SDL_SystemRAM = (int)(stat.ullTotalPhys / (1024 * 1024));
             }
+        }
+#endif
+#ifdef __AMIGAOS4__
+        if (SDL_SystemRAM <= 0) {
+            SDL_SystemRAM = IExec->AvailMem(MEMF_TOTAL) / (1024 * 1024);
         }
 #endif
     }
