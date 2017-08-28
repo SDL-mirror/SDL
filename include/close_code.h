313a2f5004b051a811bd9c48b4f8e53f93bf6b30
Sam Lantinga <slouken@libsdl.org>
2017-08-28 09:38:12 -0700

    Fixed bug 3781 - unbalanced #pragma pack(pop) in close_code.h
    Ozkan Sezer
    
    Revision 288 (http://hg.libsdl.org/SDL/rev/2f5a6062db86) excluded the
    Watcom compiler from forcing 4 byte structure packing in begin_code.h.
    However, it missed updating close_code.h, which now has an unbalanced
    #pragma pack(pop) if the compiler is Watcom.  The issue seems to have
    crawled into SDL2, too.
    
    --HG--
    branch : SDL-1.2

--- include/close_code.h
+++ include/close_code.h
@@ -32,7 +32,7 @@
  *  @file close_code.h
  *  Reset structure packing at previous byte alignment
  */
-#if defined(_MSC_VER) || defined(__MWERKS__) || defined(__WATCOMC__)  || defined(__BORLANDC__)
+#if defined(_MSC_VER) || defined(__MWERKS__) || defined(__BORLANDC__)
 #ifdef __BORLANDC__
 #pragma nopackwarning
 #endif
