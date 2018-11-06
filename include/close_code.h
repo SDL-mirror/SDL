31ff6ca18568f16a90dd9ec39696fefb2d15a3d9
Ozkan Sezer <sezeroz@gmail.com>
2018-11-06 23:45:04 +0300

    close_code.h: #error if included without matching begin_code.h
    --HG--
    branch : SDL-1.2

--- include/close_code.h
+++ include/close_code.h
@@ -26,12 +26,12 @@
  *  after you finish any function and structure declarations in your headers
  */
 
+#ifndef _begin_code_h
+#error close_code.h included without matching begin_code.h
+#endif
 #undef _begin_code_h
 
-/**
- *  @file close_code.h
- *  Reset structure packing at previous byte alignment
- */
+/* Reset structure packing at previous byte alignment */
 #if defined(_MSC_VER) || defined(__MWERKS__) || defined(__BORLANDC__)
 #ifdef __BORLANDC__
 #pragma nopackwarning
